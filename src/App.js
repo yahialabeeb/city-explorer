c17ae4edc4252813815f7f35f4e2e1506c5c78fa
Yahialabeeb <yahia.labeeb@gmail.com>
2021-09-06 23:09:28 +0300

    lab07

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Weather from './component/Weather.js'
+
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,7 +15,8 @@ class App extends React.Component {
       locName: ' ',
       showData: false,
       showError: false,
-      mapURL: ''
+      mapURL: '',
+      show: false
     }
   }
   getData = async (e) => {
@@ -29,55 +32,72 @@ class App extends React.Component {
       await this.setState({
         showData: false,
         showError: true
-      }) 
-     }
-  
-  
- 
+      })
+    }
+
+
+
     console.log('s', `${process.env.REACT_APP_LOCATIONIQ_KEY}`);
-      let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.locName}&format=json`;
+    let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.locName}&format=json`;
 
-      let comingData = await axios.get(locURL)
+    let comingData = await axios.get(locURL)
 
 
-      console.log('s', comingData.data[0]);
+    console.log('s', comingData.data[0]);
 
-      await this.setState({
-        cityData: comingData.data[0],
-        showData: true
+    await this.setState({
+      cityData: comingData.data[0],
+      showData: true
 
-      })
-      let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&format=json`
+    })
+    let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&format=json`
 
-      await this.setState({
-        mapURL: mapUrl
-      })
-    }
-  
+    await this.setState({
+      mapURL: mapUrl,
+      showError: false,
+      show: true
+    })
 
-    render() {
 
-      return (
-        <>
-          <h2>
-            City explorer
-          </h2>
-          <form onSubmit={this.getData}>
-            <input type="text" name="cityname" placeholder="enter city name" />
-            <button>
-              show location
-            </button>
-          </form>
-
-          {this.state.showData &&
-            <p>{this.state.cityData.display_name} : Lat:{this.state.cityData.lat} / Lon:{this.state.cityData.lon} </p>
-          }
-          {this.state.showData &&
-            <img src={this.state.mapURL} alt="anything" />
-          }
-          {this.state.showError && <p>Sorry Error</p>}
-        </>
-      );
-    }
   }
+
+  manger = () => {
+    console.log(this.state.show)
+    this.setState({
+      show: false
+    })
+  }
+  render() {
+
+    return (
+      <>
+        <h2>
+          City explorer
+        </h2>
+        <form onSubmit={this.getData}>
+          <input type="text" name="cityname" placeholder="enter city name" />
+          <button>
+            Find city
+          </button>
+        </form>
+
+        {this.state.showData &&
+          <p>{this.state.cityData.display_name} : Lat:{this.state.cityData.lat} / Lon:{this.state.cityData.lon} </p>
+        }
+        {this.state.showData &&
+          <img src={this.state.mapURL} alt="anything" />
+        }
+        {this.state.showError && <p>Sorry Error</p>}
+
+        <Weather
+          cityData={this.state.cityData}
+          locName={this.state.locName}
+          showData={this.state.showData}
+          manger={this.manger}
+          show={this.state.show}
+        />
+      </>
+    );
+  }
+}
 export default App;
